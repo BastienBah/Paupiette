7009ea48cbedc87b3fd7fa2d94cb65caa574779c
mharribey <mathieuharribey@hotmail.fr>
2017-11-17 17:39:24 +0100

    set mailer in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,9 @@ Rails.application.configure do
     :authentication => :cram_md5
   }
 
+  config.action_mailer.default_url_options = { :host => 'www.paupiette.co' }
+
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
