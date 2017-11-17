d56a018e34f6e7e13fc1d7c667c58fc44c7cc3a4
mharribey <mathieuharribey@hotmail.fr>
2017-11-17 16:54:42 +0100

    try fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Paupiette_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'www.paupiette.co' }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
