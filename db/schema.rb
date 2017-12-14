6e0dbe2657065aaa2fd72b018c12641d9b32bf7f
jonahalmn <jonah.allemonne@icloud.com>
2017-12-14 15:59:29 +0100

    description to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120203141) do
+ActiveRecord::Schema.define(version: 20171214090430) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "sender_id"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20171120203141) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "description"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
